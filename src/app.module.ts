526a4a84e3343a4f2d79b8b2de24f1dbd192dfaa
Benoit Bourgeon <benoit.bourgeon.ext@primetals.com>
2025-07-31 13:40:23 +0200

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dbUser:test@cluster0.n3qebom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
