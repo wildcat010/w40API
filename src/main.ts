104de8d319920ac33717bd318738da1b579113db
Benoit Bourgeon <benoit.bourgeon.ext@primetals.com>
2025-08-08 16:51:23 +0200

    update CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200', // Allow Angular dev server
+    credentials: true, // If you send cookies/auth headers
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
